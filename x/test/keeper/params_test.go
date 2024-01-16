91fa879b0051e25ce18ff1819ae362d8e1508ed1
zengyuexing <1736782823@qq.com>
2024-01-16 16:01:27 +0800

    feat: add test module

---
+++ x/test/keeper/params_test.go
@@ -0,0 +1,18 @@
+package keeper_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	testkeeper "zyx/testutil/keeper"
+	"zyx/x/test/types"
+)
+
+func TestGetParams(t *testing.T) {
+	k, ctx := testkeeper.TestKeeper(t)
+	params := types.DefaultParams()
+
+	k.SetParams(ctx, params)
+
+	require.EqualValues(t, params, k.GetParams(ctx))
+}
